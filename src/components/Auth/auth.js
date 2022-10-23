29ae95a53e3006176a4b10025a6f7b5b186c9fbd
farhan sharif <farhansharif1410@gmail.com>
2022-10-23 21:53:31 +0530

    auth code added

---
+++ src/components/Auth/auth.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD6d7TcLGmi-pUGCVtAu9VP2td84mtOOho",
+    authDomain: "landing-page-80347.firebaseapp.com",
+    projectId: "landing-page-80347",
+    storageBucket: "landing-page-80347.appspot.com",
+    messagingSenderId: "688427471022",
+    appId: "1:688427471022:web:5bcb2207129429215f1d3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
